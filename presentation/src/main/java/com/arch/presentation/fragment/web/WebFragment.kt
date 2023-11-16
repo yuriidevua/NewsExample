4d1d856a4b499da1f022632536495ac8e816763a
Yuri <lechenko.yura@gmail.com>
2023-11-16 13:28:43 +0200

    web screen fix

--- presentation/src/main/java/com/arch/presentation/fragment/web/WebFragment.kt
+++ presentation/src/main/java/com/arch/presentation/fragment/web/WebFragment.kt
@@ -2,6 +2,7 @@ package com.arch.presentation.fragment.web
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebChromeClient
